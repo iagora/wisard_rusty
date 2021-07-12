d7f54e35248f4c611f6ee9b4e2f3e28f267792b7
iagora <iagora@gmail.com>
2021-07-12 16:52:35 -0300

    WIP: train implemented
    classify next!

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,8 +1,46 @@
 use image::{GrayImage, RgbImage};
 use std::collections::HashMap;
 
+pub struct Discriminator {
+    number_of_hashtables: u16,
+    h_rams: Vec<HashMap<u64, u16>>,
+    times_trained: u64,
+}
+
+impl Discriminator {
+    pub fn new(num: u16) -> Discriminator {
+        Discriminator {
+            number_of_hashtables: num,
+            h_rams: Vec::new(),
+            times_trained: 0,
+        }
+    }
+
+    pub fn train(&mut self, x: Vec<u64>) -> () {
+        for i in 0..self.number_of_hashtables {
+            let key = x[i as usize];
+            let counter = self.h_rams[i as usize].entry(key).or_insert(0);
+            *counter += 1;
+        }
+        self.times_trained += 1;
+    }
+
+    pub fn classify(self, x: Vec<u64>, bleach: u16) -> (u64, u64) {
+        let mut votes: u64 = 0;
+        for i in 0..self.number_of_hashtables {
+            let key = x[i as usize];
+            if let Some(x) = self.h_rams[i as usize].get(&key) {
+                if *x > bleach {
+                    votes += 1
+                };
+            }
+        }
+        (votes, self.times_trained)
+    }
+}
+
 pub struct Wisard {
-    discs: HashMap<String, String>,
+    discs: HashMap<String, Discriminator>,
     addr_length: u64,
     number_of_hashtables: u16,
     mapping: Vec<u64>,
@@ -11,8 +49,8 @@ pub struct Wisard {
 }
 
 impl Wisard {
-    pub fn new(number_of_hashtables: u16, addr_length: u64) -> Self {
-        Self {
+    pub fn new(number_of_hashtables: u16, addr_length: u64) -> Wisard {
+        Wisard {
             discs: HashMap::new(),
             addr_length: addr_length,
             number_of_hashtables: number_of_hashtables,
@@ -50,7 +88,8 @@ impl Wisard {
 
     pub fn train(mut self, img: GrayImage, label: String) {
         if !self.discs.contains_key(&label) {
-            self.discs.insert(label.clone(), String::from("d")); // TODO: create kubernetes pod and put the url here
+            self.discs
+                .insert(label.clone(), Discriminator::new(self.number_of_hashtables));
         }
 
         let flat_image = img.into_flat_samples().to_vec().samples;
@@ -62,8 +101,7 @@ impl Wisard {
             .collect();
         let addresses: Vec<u64> = self.ranks(samples);
         let disc = self.discs.get_mut(&label).unwrap();
-        // disc.train(addresses); // TODO: make request to Discriminator API
-        unimplemented!();
+        disc.train(addresses);
     }
 
     pub fn classiy(self, img: GrayImage) {
@@ -122,7 +160,6 @@ mod tests {
         let addresses = wis.ranks(samples);
         assert_eq!(addresses, vec![0, 1, 2, 3, 4, 5, 6, 7, 8]);
     }
-
     #[test]
     fn test_rank_differente_addresses() {
         let mut wis = Wisard::new(28, 8);
