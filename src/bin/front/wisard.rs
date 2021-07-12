b62de0f560bce3f4848d643443e6c553368e12a1
iagora <iagora@gmail.com>
2021-07-12 14:50:05 -0300

    fix: ranks wasn't really finished

--- src/bin/front/wisard.rs
+++ src/bin/front/wisard.rs
@@ -1,12 +1,13 @@
 use image::{Rgb, RgbImage};
+use std::collections::HashMap;
 
 pub struct Wisard {
     discs: Vec<(String, String)>,
     addr_length: u64,
     number_of_hashtables: u16,
     mapping: Vec<u64>,
-    last_rank: u64,
-    rank_tables: Vec<Vec<usize>>,
+    last_rank: usize,
+    rank_tables: HashMap<Vec<usize>, usize>,
 }
 
 impl Wisard {
@@ -17,13 +18,13 @@ impl Wisard {
             number_of_hashtables: number_of_hashtables,
             mapping: (0..addr_length as u64 * number_of_hashtables as u64).collect::<Vec<u64>>(),
             last_rank: 0,
-            rank_tables: Vec::new(),
+            rank_tables: HashMap::new(),
         }
     }
 
-    fn ranks<T: PartialOrd + Clone>(&mut self, samples: Vec<T>) {
-        // find a way to change to generic
+    fn ranks<T: PartialOrd + Clone>(&mut self, samples: Vec<T>) -> Vec<usize> {
         let mut vetor = Vec::with_capacity(self.addr_length as usize);
+        let mut addresses = Vec::new();
         for i in (0..samples.len()).step_by(self.addr_length as usize) {
             if i + self.addr_length as usize <= samples.len() {
                 vetor.append(&mut samples[i..i + self.addr_length as usize].to_vec());
@@ -31,11 +32,16 @@ impl Wisard {
                 vetor.append(&mut samples[i..samples.len()].to_vec());
             }
             let mut tuples: Vec<(usize, &T)> = vetor.iter().enumerate().collect();
-            tuples.sort_by(|a, b| a.1.partial_cmp(b.1).unwrap());
-            let adress: Vec<usize> = tuples.iter().map(|a| a.0).collect();
-            self.rank_tables.push(adress);
+            tuples.sort_by(|a, b| a.1.partial_cmp(b.1).unwrap()); // TODO: treat the Option
+            let address: Vec<usize> = tuples.iter().map(|a| a.0).collect();
+            if !self.rank_tables.contains_key(&address) {
+                self.rank_tables.insert(address.clone(), self.last_rank);
+                self.last_rank += 1;
+            }
+            addresses.push(*self.rank_tables.get(&address).unwrap()); // TODO: treat the Option
             vetor.clear();
         }
+        addresses
     }
 
     pub fn train(self, img: RgbImage, label: String) {
@@ -63,5 +69,39 @@ mod tests {
         ];
         wis.ranks(samples);
         assert!(!wis.rank_tables.is_empty());
+    }
+    #[test]
+    fn test_rank_table_length() {
+        let mut wis = Wisard::new(28, 8);
+        let samples = vec![
+            52, 70, 64, 199, 7, 133, 5, 194, 16, 104, 41, 147, 42, 77, 188, 140, 148, 160, 6, 87,
+            107, 73, 168, 95, 63, 11, 2, 49, 130, 43, 92, 110, 13, 157, 125, 6, 93, 119, 86, 85,
+            103, 27, 124, 65, 9, 195, 21, 130, 192, 32, 136, 34, 70, 89, 84, 167, 175, 148, 116,
+            177, 161, 134, 98, 30, 190, 47,
+        ];
+        wis.ranks(samples);
+        let length1 = wis.rank_tables.len();
+        let samples = vec![
+            52, 70, 64, 199, 7, 133, 5, 194, 16, 104, 41, 147, 42, 77, 188, 140, 148, 160, 6, 87,
+            107, 73, 168, 95, 63, 11, 2, 49, 130, 43, 92, 110, 13, 157, 125, 6, 93, 119, 86, 85,
+            103, 27, 124, 65, 9, 195, 21, 130, 192, 32, 136, 34, 70, 89, 84, 167, 175, 148, 116,
+            177, 161, 134, 98, 30, 190, 47,
+        ];
+        wis.ranks(samples);
+        let length2 = wis.rank_tables.len();
+        assert_eq!(length1, length2);
+    }
+
+    #[test]
+    fn test_rank_addresses() {
+        let mut wis = Wisard::new(28, 8);
+        let samples = vec![
+            52, 70, 64, 199, 7, 133, 5, 194, 16, 104, 41, 147, 42, 77, 188, 140, 148, 160, 6, 87,
+            107, 73, 168, 95, 63, 11, 2, 49, 130, 43, 92, 110, 13, 157, 125, 6, 93, 119, 86, 85,
+            103, 27, 124, 65, 9, 195, 21, 130, 192, 32, 136, 34, 70, 89, 84, 167, 175, 148, 116,
+            177, 161, 134, 98, 30, 190, 47,
+        ];
+        let addresses = wis.ranks(samples);
+        assert_eq!(addresses, vec![0, 1, 2, 3, 4, 5, 6, 7, 8]);
     }
 }
